ff52f5b00539763abf9c5693530dcaa48d92349c
Sebastian Manzano <samanzanom@gmail.com>
2024-02-11 20:16:58 +0000

    github dockerize app

--- src/test/java/cl/previred/challenge/SingletonPostgresContainer.java
+++ src/test/java/cl/previred/challenge/SingletonPostgresContainer.java
@@ -7,8 +7,21 @@ public class SingletonPostgresContainer {
     private static final PostgreSQLContainer<?> postgresContainer;
 
     static {
-        postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13"));
+        postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13"))
+                .withDatabaseName("testdb")
+                .withUsername("test")
+                .withPassword("test");
         postgresContainer.start();
+
+        // Obtener la URL de conexión dinámica
+        String jdbcUrl = postgresContainer.getJdbcUrl();
+        String username = postgresContainer.getUsername();
+        String password = postgresContainer.getPassword();
+
+        // Configurar la aplicación o el framework de test para usar estos valores
+        System.setProperty("spring.datasource.url", jdbcUrl);
+        System.setProperty("spring.datasource.username", username);
+        System.setProperty("spring.datasource.password", password);
     }
 
     public static PostgreSQLContainer<?> getInstance() {
